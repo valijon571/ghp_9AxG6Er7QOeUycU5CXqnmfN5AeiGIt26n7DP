3bb8f7c26ff12d62e449f3faacc714b04394a0a4
valijon571 <qarshiboyevv85@gmail.com>
2023-11-28 10:47:53 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import Api from "./appe/Api";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+// import { Provider } from "react-redux";
+// import createStore from "./helper/store";
 
 function App() {
+  // const store = createStore();
   return (
     <>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
     </>
   );
 }
